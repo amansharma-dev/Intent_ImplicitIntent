70baab485cff8b8f7d6bb927f8feef8f7c68f65b
Aman Sharma <amansharma.11@hotmail.com>
2019-05-05 17:04:19 +0530

    commit

--- app/src/main/java/com/example/intent_implicitintent/MainActivity.java
+++ app/src/main/java/com/example/intent_implicitintent/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.intent_implicitintent;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText location;
+    private EditText shareText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        location = findViewById(R.id.edittext_openlocation);
+        shareText = findViewById(R.id.edittext_sharetext);
+    }
+
+
+    public void openLocation(View view){
+
+        String loc = location.getText().toString();
+
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Toast.makeText(getApplicationContext(),"Can't handle this intent!",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void shareText(View view){
+
+        String txt = shareText.getText().toString();
+        String mimeType = "text/plain";
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle(R.string.text_share_text_with)
+                .setText(txt)
+                .startChooser();
     }
 }
